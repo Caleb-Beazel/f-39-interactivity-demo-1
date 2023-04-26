9472a4315b5978ea42363239fc10036d26051678
Caleb Beazel <caleb.beazel@gmail.com>
2023-04-26 15:33:34 -0600

    v2-completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const minusButton = document.getElementById('minus-btn')
 const resetButton = document.getElementById('reset-btn')
 const plusButton = document.getElementById('plus-btn')
 const counterElement = document.getElementById('counter')
+const buttonList = document.querySelectorAll('.theme-buttons')
 
 let count = 0
 
@@ -19,3 +20,27 @@ minusButton.addEventListener('click', () => {
     count--
     counterElement.innerHTML = count
 })
+
+
+
+const themeButtonClickHandler = (event) => {
+    let themeButton = event.target
+    let theme = themeButton.innerHTML
+
+    let body = document.querySelector('body')
+    let main = document.querySelector('main')
+    let allButtons = document.querySelectorAll('button')
+
+    body.classList = ""
+    body.classList.add(theme)
+    main.classList = ""
+    main.classList.add(theme)
+    for (i = 0; i < allButtons.length; i++) {
+        allButtons[i].classList = ""
+        allButtons[i].classList.add(theme)
+    }
+}
+
+for (i = 0; i < buttonList.length; i++){
+    buttonList[i].addEventListener('click', themeButtonClickHandler)
+}
